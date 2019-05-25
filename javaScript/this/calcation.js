9fcb5e900af0e7e30e90c9822ca75e9cd2337cc0
koskas <koskas@yahoo.com>
2019-05-25 21:02:09 +0300

    test

--- javaScript/this/calcation.js
+++ javaScript/this/calcation.js
@@ -3,36 +3,42 @@
 // "this" keyword -and- function reference
 //
 
-// temp-list: https://academind.com/learn/javascript/this-keyword-function-references/
+// temp-list: 
+// https://www.youtube.com/watch?time_continue=526&v=Pv9flm-80vM
+// https://academind.com/learn/javascript/this-keyword-function-references/
 
-//instance gen to NameGererator()
-const gen = new NameGenerator(); //object = new instance
 
-
-class NameField() {
+class NameField {
     constructor(name)
     {
         const field = document.createElement('li'); //field item
         field.textContent = name; //text fo field item + argument from the constructor
-
-        const nameListHook = doucment.querySelector('#names'); //reference to unorder-list with id="name"
+        const nameListHook = document.querySelector('#names'); //reference to unorder-list with id="name"
         nameListHook.appendChild(field); //ading the name to list.
     }
 }
 
 // class for the object
 class NameGenerator {
-    //constructor
     constructor() {
         //refernece to a "button"
-        const btn = document.querySelector('button');
+        const btn = document.querySelector('button'); 
         //event listener with "click" event.
-        btn.addEventListener('click',???); //when 'click' do somthing
+
+        // btn.addEventListener('click',addName); 
+        //  -!- error message: addName is not defined 
+        //  -!- fixed: "this." gives us access to object "NameGernerator", and then we can call method
+        // btn.addEventListener('click',this.addName()); // method will be execute one on html/javascript load "addName()""
+        btn.addEventListener('click', this.addName); // call a reference of the method "addName", now the event will work.
+
     }
 
-    //function/method in the class
+    //function/method in the class (attached to the object)
     addName() {
         //instance "name" to NameField()
-        const name = new NameField("Test");  //object = new instance
+        const name = new NameField("jron");  //object = new instance
     }
 }
+
+//instance gen to NameGererator()
+const gen = new NameGenerator(); //object = new instance

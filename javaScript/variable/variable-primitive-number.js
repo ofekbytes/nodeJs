278c550df8b2b211958ef20724cef6d3ae7bb7af
yaron <koskas@yahoo.com>
2019-06-08 08:40:29 +0300

    test

---
+++ javaScript/variable/variable-primitive-number.js
@@ -0,0 +1,16 @@
+//
+// primitive variable - number
+//
+
+//var pi = 3.14159265359;
+var number1=10;
+var number2=20;
+var result = 0;
+
+console.log ("pi ==> " + pi);
+
+console.log ("number1 ==> " + number1);
+console.log ("number2 ==> " + number2);
+console.log ("number3 ==> " + number3);
+
+console.log( 3 ** 2);

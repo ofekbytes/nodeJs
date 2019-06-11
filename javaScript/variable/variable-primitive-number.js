9ad731991b252b018d9584e8fb50571f94c420c2
yaron <koskas@yahoo.com>
2019-06-11 12:06:03 +0300

    examples

--- javaScript/variable/variable-primitive-number.js
+++ javaScript/variable/variable-primitive-number.js
@@ -2,15 +2,17 @@
 // primitive variable - number
 //
 
-//var pi = 3.14159265359;
+var pi = 3.14159265359;
 var number1=10;
 var number2=20;
 var result = 0;
 
-console.log ("pi ==> " + pi);
+result = number1 + number2;
 
-console.log ("number1 ==> " + number1);
-console.log ("number2 ==> " + number2);
-console.log ("number3 ==> " + number3);
+console.log ('pi ==> ' + pi);
 
-console.log( 3 ** 2);
+console.log ('number1 ==> ' + number1);
+console.log ('number2 ==> ' + number2);
+console.log ('number3 ==> ' + result);
+
+console.log('3 ^ 2 ==> ' +  (3 ** 2) );
